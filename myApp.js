54b440003c08b00bcad797446d24c8540143677d
crazyalbertw <albert.wby@gmail.com>
2016-01-06 15:19:10 -0800

    edit navbar

---
+++ myApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by crazyalbert on 1/6/16.
+ */
+var app = angular.module("myApp", []);
---
+++ myCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by crazyalbert on 1/6/16.
+ */
+app.controller("myCtrl", function($scope) {
+    $scope.firstname = "albert";
+    $scope.lastname = "wang";
+});
